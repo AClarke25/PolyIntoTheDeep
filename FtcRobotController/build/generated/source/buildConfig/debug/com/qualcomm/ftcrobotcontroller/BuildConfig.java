b442d519c2d02af4211b7af211c93f45967c8059
JWodos <woodsj27@polyprep.org>
2024-10-16 16:46:06 -0400

    Update

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,9 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
+<<<<<<< Updated upstream
   public static final String APP_BUILD_TIME = "2024-10-10T16:39:35.006-0400";
+=======
+  public static final String APP_BUILD_TIME = "2024-10-01T17:08:35.480-0400";
+>>>>>>> Stashed changes
 }
